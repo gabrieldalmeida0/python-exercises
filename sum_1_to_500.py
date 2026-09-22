8c64ae4c2382734db5ad842fef55ac6c91303a04
Gabriel D'Almeida <gabrieljoseveras@gmail.com>
2026-09-21 23:58:14 -0300

    Organiza exercício de soma de 1 a 500

---
+++ sum_1_to_500.py
@@ -0,0 +1,11 @@
+total = 0
+count = 0
+current = 1
+
+while current <= 500:
+    total += current
+    count += 1
+    current += 1
+
+print("Soma de 1 até 500:", total)
+print("Quantidade de números:", count)
